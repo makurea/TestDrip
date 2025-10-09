0d3d6cbecbe3dae0315deb6c5e8e5f9f388df49f
makurea <makurea@outlook.com>
2025-10-09 21:33:32 +0300

    add code logger

--- src/test/java/com/testdrip/steps/SpaceXCapsulesSteps.java
+++ src/test/java/com/testdrip/steps/SpaceXCapsulesSteps.java
@@ -1,6 +1,8 @@
 // src/test/java/com/testdrip/steps/SpaceXCapsulesSteps.java
 package com.testdrip.steps;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
@@ -10,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SpaceXCapsulesSteps {
 
+  private static final Logger logger = LogManager.getLogger(SpaceXCapsulesSteps.class);
   private String baseUri;
   private Response response;
 
@@ -17,12 +20,12 @@ public class SpaceXCapsulesSteps {
   public void theSpaceXAPIBaseURI(String uri) {
     this.baseUri = uri;
     RestAssured.baseURI = baseUri;
-    System.out.println("Base URI set to: " + baseUri);
+    logger.info("Base URI set to: {}", baseUri);
   }
 
   @When("I request capsule details for serial {string}")
   public void iRequestCapsuleDetailsForSerial(String capsuleSerial) {
-    System.out.println("Requesting details for capsule serial: " + capsuleSerial);
+    logger.debug("Requesting details for capsule serial: {}", capsuleSerial);
     response = RestAssured.given()
         .pathParam("capsule_serial", capsuleSerial)
         .get("/capsules/{capsule_serial}");
@@ -31,40 +34,36 @@ public class SpaceXCapsulesSteps {
 
   @Then("the response status code is {int}")
   public void theResponseStatusCodeIs(int expectedStatusCode) {
-    System.out.println("Verifying status code...");
     assertThat(response.statusCode())
         .as("Status code should be " + expectedStatusCode)
         .isEqualTo(expectedStatusCode);
-    System.out.println("Status code is " + expectedStatusCode);
+    logger.info("Status code is {}", expectedStatusCode);
   }
 
   @Then("the capsule serial in the response is {string}")
   public void theCapsuleSerialInTheResponseIs(String expectedSerial) {
-    System.out.println("Verifying capsule serial...");
     String actualSerial = response.jsonPath().getString("capsule_serial");
     assertThat(actualSerial)
         .as("Capsule serial should be " + expectedSerial)
         .isEqualTo(expectedSerial);
-    System.out.println("Capsule serial is " + actualSerial);
+    logger.info("Capsule serial is {}", actualSerial);
   }
 
   @Then("the capsule status is {string}")
   public void theCapsuleStatusIs(String expectedStatus) {
-    System.out.println("Verifying capsule status...");
     String actualStatus = response.jsonPath().getString("status");
     assertThat(actualStatus)
         .as("Capsule status should be " + expectedStatus)
         .isEqualTo(expectedStatus);
-    System.out.println("Capsule status is " + actualStatus);
+    logger.info("Capsule status is {}", actualStatus);
   }
 
   @Then("the capsule type is {string}")
   public void theCapsuleTypeIs(String expectedType) {
-    System.out.println("Verifying capsule type...");
     String actualType = response.jsonPath().getString("type");
     assertThat(actualType)
         .as("Capsule type should be " + expectedType)
         .isEqualTo(expectedType);
-    System.out.println("Capsule type is " + actualType);
+    logger.info("Capsule type is {}", actualType);
   }
 }
